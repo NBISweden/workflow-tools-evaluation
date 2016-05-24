4365864cc8e10bef24283f5ac62f873bf6b0336f
Samuel Lampa <samuel.lampa@gmail.com>
2016-05-24 17:10:28 +0200

    Add comments in SciPipe workflow

--- 002-resequencing/resequencing_scipipe.go
+++ 002-resequencing/resequencing_scipipe.go
@@ -11,18 +11,23 @@ const (
 )
 
 func main() {
+	// Generator for the two reads
 	pairsGen := scipipe.NewStringGenerator("1", "2")
 
+	// Download FastQ component
 	dlFastq := scipipe.Shell("dl_fastq", "wget -O {o:fastq} "+fastq_base_url+fmt.Sprintf(fastq_base_name, "{p:pair}"))
 	dlFastq.PathFormatters["fastq"] = func(t *scipipe.SciTask) string {
 		return fmt.Sprintf(fastq_base_name, t.Params["pair"])
 	}
 
+	// Sink component
 	sink := scipipe.NewSink()
 
+	// Specify data flow
 	dlFastq.ParamPorts["pair"] = pairsGen.Out
 	sink.In = dlFastq.OutPorts["fastq"]
 
+	// Set up and run
 	pipeRun := scipipe.NewPipelineRunner()
 	pipeRun.AddProcs(pairsGen, dlFastq, sink)
 	pipeRun.Run()
