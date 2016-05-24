f63504e51e86d1cecec113cfc7b46c80cb238cdc
Samuel Lampa <samuel.lampa@gmail.com>
2016-05-24 17:06:33 +0200

    Simplify SciPipe workflow with new StringGenerator component

--- 002-resequencing/resequencing_scipipe.go
+++ 002-resequencing/resequencing_scipipe.go
@@ -11,22 +11,19 @@ const (
 )
 
 func main() {
+	pairsGen := scipipe.NewStringGenerator("1", "2")
+
 	dlFastq := scipipe.Shell("dl_fastq", "wget -O {o:fastq} "+fastq_base_url+fmt.Sprintf(fastq_base_name, "{p:pair}"))
 	dlFastq.PathFormatters["fastq"] = func(t *scipipe.SciTask) string {
 		return fmt.Sprintf(fastq_base_name, t.Params["pair"])
 	}
+
 	sink := scipipe.NewSink()
 
+	dlFastq.ParamPorts["pair"] = pairsGen.Out
 	sink.In = dlFastq.OutPorts["fastq"]
 
-	go func() {
-		defer close(dlFastq.ParamPorts["pair"])
-		for i := 1; i <= 2; i++ {
-			dlFastq.ParamPorts["pair"] <- fmt.Sprintf("%d", i)
-		}
-	}()
-
 	pipeRun := scipipe.NewPipelineRunner()
-	pipeRun.AddProcs(dlFastq, sink)
+	pipeRun.AddProcs(pairsGen, dlFastq, sink)
 	pipeRun.Run()
 }
